dc208675e935832e75c48e0e2a8a502b60551adf
Dani <dcordobar@gmail.com>
2013-02-13 19:59:19 +0100

    Initial commit

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,4 +34,17 @@ DemoApp::Application.configure do
 
   # Expands the lines which load the assets
   config.assets.debug = true
+
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+    :address => "smtp.gmail.com",
+    :port => 587,
+    :domain => "domain.of.sender.net",
+    :authentication => "plain",
+    :user_name => "dcordobar",
+    :password => "digitales",
+    :enable_starttls_auto => true
+}
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,26 @@
 DemoApp::Application.routes.draw do
-  resources :products
+  
+  get 'admin' => 'admin#index'
+
+  controller :sessions do
+    get 'login' => :new
+    post 'login' => :create
+    delete 'logout' => :destroy
+  end
+
+
+  resources :users
+  resources :orders
+  resources :line_items
+  resources :carts
+
+  get "store/index"
+
+  resources :products do
+    get :who_bought, :on => :member
+  end
+
+  #resources :products
 
 
   # The priority is based upon order of creation:
@@ -51,7 +72,7 @@ DemoApp::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'store#index', :as => 'store'
 
   # See how all your routes lay out with "rake routes"
 
